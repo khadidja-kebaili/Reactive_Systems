a0f11433b3bfd7087860e83dc36435f729f1cb39
Nick Nowak <nick.nowak@hs-furtwangen.de>
2023-11-09 11:06:32 +0100

    Added Model Tests

--- src/test/scala/model/AirplaneTest.scala
+++ src/test/scala/model/AirplaneTest.scala
@@ -6,7 +6,14 @@ import org.scalatest.wordspec.AnyWordSpec
 class AirplaneTest extends AnyWordSpec with Matchers {
 
   "Airplane" should {
-
+    "have the correct name" in {
+      val airplane = Airplane("Boeing 747")
+      airplane.name shouldEqual "Boeing 747"
+    }
+    "have a meaningful toString representation" in {
+      val airplane = Airplane("Airbus A380")
+      airplane.toString shouldEqual "Airbus A380"
+    }
   }
 
 }
--- src/test/scala/model/AirportTest.scala
+++ src/test/scala/model/AirportTest.scala
@@ -1,4 +1,4 @@
-package controller
+package model
 
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
@@ -6,7 +6,14 @@ import org.scalatest.wordspec.AnyWordSpec
 class AirportTest extends AnyWordSpec with Matchers {
 
   "Airport" should {
-
+    "have the correct name" in {
+      val airport = Airport("Frankfurt am Main")
+      airport.name shouldEqual "Frankfurt am Main"
+    }
+    "have a meaningful toString representation" in {
+      val airport = Airport("Berlin")
+      airport.toString shouldEqual "Berlin"
+    }
   }
 
 }
