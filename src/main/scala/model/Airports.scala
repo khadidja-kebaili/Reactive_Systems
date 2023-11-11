676be6299d9b5e3091f3fc66fb785957c5aeaa6f
Nick Nowak <nick.nowak@htwg-konstanz.de>
2023-11-11 22:44:07 +0100

    Created Airports model

---
+++ src/main/scala/model/Airports.scala
@@ -0,0 +1,3 @@
+package model
+
+case class Airports(airports: List[Airport])
---
+++ src/test/scala/model/AirportsTest.scala
@@ -0,0 +1,11 @@
+package model
+
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+class AirportsTest extends AnyWordSpec with Matchers {
+  "Airports" should {
+
+  }
+}
+
