8463dde3756f574ae54c8379b12af75bb7d36bca
Nick Nowak <nick.nowak@htwg-konstanz.de>
2023-11-12 14:06:00 +0100

    Added Generator tests

--- build.sbt
+++ build.sbt
@@ -15,9 +15,11 @@ lazy val root = (project in file("."))
     libraryDependencies += "org.scalatest" %% "scalatest" % "3.2.17" % "test",
     libraryDependencies += "org.scala-lang.modules" %% "scala-parser-combinators" % "2.2.0",
     libraryDependencies += "com.typesafe.akka" %% "akka-http" % AkkaHttpVersion,
+    libraryDependencies += "com.typesafe.akka" %% "akka-http-testkit" % AkkaHttpVersion,
     libraryDependencies += "com.typesafe.akka" %% "akka-actor-typed" % AkkaVersion,
     libraryDependencies += "com.typesafe.akka" %% "akka-http-spray-json" % AkkaHttpVersion,
     libraryDependencies += "com.typesafe.akka" %% "akka-stream" % AkkaVersion,
+    libraryDependencies += "com.typesafe.akka" %% "akka-stream-testkit" % AkkaVersion,
     libraryDependencies += "ch.qos.logback" % "logback-classic" % "1.4.11" % Runtime
   )
 
--- src/test/scala/GeneratorTest.scala
+++ src/test/scala/GeneratorTest.scala
@@ -1,7 +1,53 @@
+import akka.http.scaladsl.model.*
+import akka.http.scaladsl.server.*
+import akka.http.scaladsl.server.Directives.*
+import akka.http.scaladsl.testkit.ScalatestRouteTest
+import model.*
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
+import spray.json.RootJsonFormat
 
 
-class GeneratorTest extends AnyWordSpec with Matchers {
+class GeneratorTest extends AnyWordSpec with Matchers with ScalatestRouteTest {
+  "ModelJsonConverters" should {
+    "provide implicit RootJsonFormat for Airplane" in {
+      val converters = new ModelJsonConverters
+      val airplaneFormat: RootJsonFormat[Airplane] = converters.airplaneFormat
+      assert(airplaneFormat != null)
+    }
 
+    "provide implicit RootJsonFormat for Airport" in {
+      val converters = new ModelJsonConverters
+      val airportFormat: RootJsonFormat[Airport] = converters.airportFormat
+      assert(airportFormat != null)
+    }
+
+    "provide implicit RootJsonFormat for Airports" in {
+      val converters = new ModelJsonConverters
+      val airportsFormat: RootJsonFormat[Airports] = converters.airportsFormat
+      assert(airportsFormat != null)
+    }
+  }
+
+
+  "Generator" should {
+    "return some airports for GET requests to the '/airport' path" in {
+      Get("/airport") ~> Generator.routes ~> check {
+        status should ===(StatusCodes.OK)
+        contentType should ===(ContentTypes.`application/json`)
+        entityAs[String] should include regex "\"airports\":\\["
+      }
+    }
+
+    "return the right airport for GET requests to the '/airport/name' path" in {
+      Get("/airport/Frankfurt") ~> Generator.routes ~> check {
+        status should ===(StatusCodes.OK)
+        contentType should ===(ContentTypes.`application/json`)
+        entityAs[String] should include regex "\"arrivals\":\\["
+        entityAs[String] should include regex "\"departures\":\\["
+        entityAs[String] should include regex "\"name\""
+      }
+    }
+
+  }
 }
