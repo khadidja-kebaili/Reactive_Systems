25e621b6976396b2b9d39099c7e0f4526fb219c7
Nick Nowak <nick.nowak@htwg-konstanz.de>
2023-11-28 07:32:30 +0100

    Updated Airports model

--- src/main/scala/model/Airports.scala
+++ src/main/scala/model/Airports.scala
@@ -1,11 +1,3 @@
 package model
 
-case class Airports(airports: List[String]) {
-  override def toString = {
-    "Airports\n" + "-" * 30 + "\n" + airports.sorted.grouped(3).map { (row) =>
-      row.map((airport) => {
-        f"%%-20s".format(airport)
-      }).mkString
-    }.toList.mkString("\n")
-  }
-}
+case class Airports(names: List[String])
--- src/test/scala/model/AirportsTest.scala
+++ src/test/scala/model/AirportsTest.scala
@@ -7,22 +7,15 @@ class AirportsTest extends AnyWordSpec with Matchers {
   "Airports" should {
     val testAirports = List("Berlin-Tegel", "München", "Dortmund", "Erfurt-Weimar", "Stuttgart")
     val element = Airports(testAirports)
-    val list = element.airports.sorted
+    val list = element.names.sorted
     "have the correct list of airport names" in {
-      val list = Airports(testAirports).airports.sorted
+      val list = Airports(testAirports).names.sorted
       list shouldBe a[List[String]]
       list(0) equals testAirports(0)
       list(1) equals testAirports(1)
       list(2) equals testAirports(2)
       list(3) equals testAirports(3)
       list(4) equals testAirports(4)
-    }
-    "have the correct toString representation" in {
-      val text = element.toString
-      text shouldBe a[String]
-      text should startWith("Airports\n" + "-" * 30 + "\n" + list(0))
-      text should include(element.airports(2))
-      text should endWith(f"%%-20s".format(list.last))
     }
   }
 }
