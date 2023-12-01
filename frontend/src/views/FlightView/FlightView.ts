4d3d11c9dc92e4cb811653dc1119ce1ec2f31428
Nick Nowak <nick.nowak@htwg-konstanz.de>
2023-12-01 13:37:18 +0100

    Implemented FlightView

--- frontend/src/views/FlightView/FlightView.ts
+++ frontend/src/views/FlightView/FlightView.ts
@@ -1,7 +1,45 @@
+import { getFlight } from "@/services/flightService";
+import { onMounted, ref } from "vue";
+import { useRoute } from "vue-router";
+
 export default {
   name: "Flight",
   data() {
     return {};
   },
-  setup() {},
+  setup() {
+    const route = useRoute();
+    const flightNumber = ref(route.params.id.toString());
+    const airlineName = ref("");
+    const from = ref("");
+    const to = ref("");
+    const gate = ref("");
+    const arrivalTime = ref("");
+    const estimatedArrivalTime = ref("");
+
+    onMounted(async () => {
+      await getFlight(flightNumber.value)
+        .then((response) => {
+          airlineName.value = response.airlineName;
+          from.value = response.from;
+          to.value = response.to;
+          gate.value = response.gate;
+          arrivalTime.value = response.arrivalTime;
+          estimatedArrivalTime.value = response.estimatedArrivalTime;
+        })
+        .catch((error) => {
+          console.log("Error on getAirports()", error);
+        });
+    });
+
+    return {
+      flightNumber,
+      airlineName,
+      from,
+      to,
+      gate,
+      arrivalTime,
+      estimatedArrivalTime,
+    };
+  },
 };
