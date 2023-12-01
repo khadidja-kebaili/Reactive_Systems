6998cc870dc6238a144944600159c0f854f5b779
Nick Nowak <nick.nowak@htwg-konstanz.de>
2023-12-01 13:37:03 +0100

    Implemented AirportsView

--- frontend/src/views/AirportsView/AirportsView.ts
+++ frontend/src/views/AirportsView/AirportsView.ts
@@ -1,7 +1,24 @@
+import { getAllAirports } from "@/services/airportService";
+import { onMounted, ref } from "vue";
+
 export default {
   name: "AirportsView",
   data() {
     return {};
   },
-  setup() {},
+  setup() {
+    const airports = ref();
+
+    onMounted(async () => {
+      await getAllAirports()
+        .then((response) => {
+          airports.value = response;
+        })
+        .catch((error) => {
+          console.log("Error on getAirports()", error);
+        });
+    });
+
+    return { airports };
+  },
 };
