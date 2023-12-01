b1a0f776a8ae9ea6ff8a43866d3843d503cbaca2
Nick Nowak <nick.nowak@htwg-konstanz.de>
2023-12-01 13:37:29 +0100

    Implemented FlightsView

--- frontend/src/views/FlightsView/FlightsView.ts
+++ frontend/src/views/FlightsView/FlightsView.ts
@@ -1,7 +1,30 @@
+import router from "@/router";
+import { getAllFlights } from "@/services/flightService";
+import { onMounted, ref } from "vue";
+
 export default {
   name: "Flights",
   data() {
     return {};
   },
-  setup() {},
+  setup() {
+    const flights = ref();
+
+    onMounted(async () => {
+      await getAllFlights()
+        .then((response) => {
+          flights.value = response;
+        })
+        .catch((error) => {
+          console.log("Error on getAirports()", error);
+        });
+    });
+
+    return { flights };
+  },
+  methods: {
+    goToFlight(id: String) {
+      router.push(`/flight/${id}`);
+    },
+  },
 };
