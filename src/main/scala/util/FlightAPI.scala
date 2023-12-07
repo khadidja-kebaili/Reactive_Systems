c1401c3e09426dfa2897a4d325340091d1a071cc
khadidja-kebaili <80825550+khadidja-kebaili@users.noreply.github.com>
2023-12-07 17:56:11 +0100

    FlightAPI  und FlightServices repariert

--- src/main/scala/util/FlightAPI.scala
+++ src/main/scala/util/FlightAPI.scala
@@ -1,28 +1,74 @@
 package util
 
-import akka.actor.ActorSystem
-import akka.actor.Status.*
+//import akka.actor.ActorSystem
+import akka.actor.typed.ActorSystem
+import akka.actor.typed.scaladsl.Behaviors
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.Directives.*
-import model.Flight
-import util.FlightServices
-import util.*
+import akka.http.scaladsl.server.Route
+//import model.{Airport, Airports, Flight}
+import spray.json.{DefaultJsonProtocol, RootJsonFormat}
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport.*
+import akka.http.scaladsl.model.StatusCodes
+import util.SprayJsonExample.system
+import akka.actor.typed.ActorSystem
+import akka.actor.typed.scaladsl.Behaviors
+import akka.http.scaladsl.Http
+import akka.Done
+import akka.http.scaladsl.server.Route
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+import spray.json.DefaultJsonProtocol._
+import spray.json.RootJsonFormat
+
+import scala.io.StdIn
+
+import scala.concurrent.ExecutionContext
+import scala.concurrent.Future
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.{ExecutionContext, Future}
+
 
 object FlightAPI extends App {
-  implicit val system: ActorSystem = ActorSystem("FlightAPI")
-  val flightService = new FlightServices()
+  //implicit val system: ActorSystem = ActorSystem("FlightAPI")
+  implicit val system: ActorSystem[_] = ActorSystem(Behaviors.empty, "FlightAPI")
+  implicit val executionContext: ExecutionContext = system.executionContext
 
-  val route = path("flights") {
-    get {
-      onComplete(flightService.getAllFlights) {
-        case Success(res) => complete(res)
-        case Failure(ex) =>
-          println(s"Request failed with: ${ex.getMessage}")
-          complete(StatusCodes.InternalServerError)
-      }
-    }
+  final case class Flight(flightNumber: Long, airlineName: String, dep: String, arr: String)
+
+  var flight_I: Flight = Flight(1, "Lufthansa", "08:10", "09:20")
+  var flight_II: Flight = Flight(2, "Air Algerie", "19:00", "19:00")
+  var flight_III: Flight = Flight(3, "Air France", "11:40", "11:43")
+  var flights: List[Flight] = List(flight_I, flight_II, flight_III)
+
+  implicit val flightFormat: RootJsonFormat[Flight] = jsonFormat4(Flight.apply)
+  implicit val flightsFormat: RootJsonFormat[List[Flight]] = listFormat[Flight]
+  def getAllFlights: Future[List[Flight]] = Future.successful(flights)
+  def getOneFlight(flightNumber: Long): Future[Option[Flight]] = Future {
+    flights.find(f => f.flightNumber == flightNumber)
   }
+
+  val route: Route =
+    concat(
+          get {
+            pathPrefix("flight" / LongNumber ) { flightNumber =>
+              val futureFlights: Future[Option[Flight]] = getOneFlight(flightNumber)
+              onComplete(futureFlights) { flight =>
+                complete(flight)
+              }
+            }
+          },
+          get {
+            path("flights") {
+            val futureFlights: Future[List[Flight]] = getAllFlights
+            onComplete(futureFlights) { flights =>
+              complete(flights)
+            }
+          }
+        }
+          )
 
   val server = Http().newServerAt("localhost", 4040).bind(route)
   server.map { _ =>
--- src/main/scala/util/FlightServices.scala
+++ src/main/scala/util/FlightServices.scala
@@ -1,6 +1,7 @@
+/*
 package util
 
-import model.Flight
+import model.{Airport, Airports, Flight}
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
@@ -9,11 +10,23 @@ import spray.json.{DefaultJsonProtocol, RootJsonFormat}
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import spray.json.DefaultJsonProtocol.*
 
-class FlightServices extends DefaultJsonProtocol with SprayJsonSupport {
-  implicit val flightFormat: RootJsonFormat[Flight] = jsonFormat4(Flight.apply)
+import util.ModelJsonConverter
+class FlightServices extends App with SprayJsonSupport with ModelJsonConverter  {
+  //implicit val flightFormat: RootJsonFormat[Flight] = jsonFormat4(Flight.apply)
 
-  var flights: List[Flight] = List(Flight("F001", "Air Algerie", "19:00", "19:10"))
+  var flight_I: Flight = Flight("F001", "Air Algerie", "19:00", "19:10")
+  var flight_II: Flight = Flight("F002", "Air Algerie", "19:00", "19:10")
+  var flight_III: Flight = Flight("F003", "Air Algerie", "19:00", "19:10")
+
+
+  val flights: List[Flight] = List(flight_I, flight_II, flight_III)
+  
   def getAllFlights: Future[List[Flight]] = Future.successful(flights)
+
+  def getOneFlight: Flight = {
+    flight_I
+  }
 
 
 }
+*/
