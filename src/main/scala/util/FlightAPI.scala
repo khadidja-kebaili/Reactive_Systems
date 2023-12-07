940e2205789620d3ab49830c9cd3d45b5ab1f7d0
khadidja-kebaili <80825550+khadidja-kebaili@users.noreply.github.com>
2023-12-07 18:08:54 +0100

    FlightAPI  und FlightServices repariert

--- src/main/scala/util/FlightAPI.scala
+++ src/main/scala/util/FlightAPI.scala
@@ -1,6 +1,5 @@
 package util
 
-//import akka.actor.ActorSystem
 import akka.actor.typed.ActorSystem
 import akka.actor.typed.scaladsl.Behaviors
 import akka.http.scaladsl.Http
@@ -11,28 +10,15 @@ import spray.json.{DefaultJsonProtocol, RootJsonFormat}
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport.*
 import akka.http.scaladsl.model.StatusCodes
 import util.SprayJsonExample.system
-import akka.actor.typed.ActorSystem
-import akka.actor.typed.scaladsl.Behaviors
-import akka.http.scaladsl.Http
 import akka.Done
-import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.server.Directives._
-import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 import spray.json.DefaultJsonProtocol._
-import spray.json.RootJsonFormat
-
-import scala.io.StdIn
-
-import scala.concurrent.ExecutionContext
-import scala.concurrent.Future
-
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.{ExecutionContext, Future}
 
 
 object FlightAPI extends App {
-  //implicit val system: ActorSystem = ActorSystem("FlightAPI")
   implicit val system: ActorSystem[_] = ActorSystem(Behaviors.empty, "FlightAPI")
   implicit val executionContext: ExecutionContext = system.executionContext
 
